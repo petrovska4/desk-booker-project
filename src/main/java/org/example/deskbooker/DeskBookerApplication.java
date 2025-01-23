68feb5f8f896896bcf9f6c90fe288f6979eb9876
petrovska4 <tijanapetrovska173@gmail.com>
2025-01-23 14:14:09 +0100

    first commit

---
+++ src/main/java/org/example/deskbooker/DeskBookerApplication.java
@@ -0,0 +1,13 @@
+package org.example.deskbooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeskBookerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeskBookerApplication.class, args);
+    }
+
+}
