99700c9961c42cbca17ee6e5f74030ee1034cd79
Bojana <pecakovab@gmail.com>
2025-06-18 16:10:08 +0200

    Add Flask notification-service microservice

---
+++ notification-service/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/notify', methods=['POST'])
+def notify():
+    data = request.get_json()
+    user = data.get("user")
+    action = data.get("action")
+    message = f"Notification received for user {user} - Action: {action}"
+    print(message)
+    return jsonify({"message": message}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
